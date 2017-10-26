4003284addc90d6722b3290ad602358da2480c66
montalbo <oseppe@users.noreply.github.com>
2017-10-26 15:55:10 +0800

    Connect sprite handler fxn to movesArbiter

---
+++ js/handleSprite.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const moveSprite = (figure, canvasWidth, canvasHeight, direction) => {
+	let lowerCaseDirection = direction.toLowerCase();
+
+	switch(lowerCaseDirection) {
+		case 'left':
+			figure.moveX('left', canvasWidth);
+			break;
+		case 'right':
+			figure.moveX('right', canvasWidth);
+			break;
+		case 'up':
+			figure.moveY('up', canvasHeight);
+			break;
+		case 'down':
+			figure.moveY('down', canvasHeight);
+			break;
+	}
+}
+
+// TODO: Remove the if
+const moveSpriteCallback = (data) => {
+	if (data['winner'] === '') return;
+	moveSprite(sprite, canvas.width, canvas.height, data['winner']);
+}    
