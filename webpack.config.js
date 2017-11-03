226a572dca7e47d5136d1a8e3b3191a781a3d2a1
montalbo <oseppe@users.noreply.github.com>
2017-11-03 10:24:31 +0800

    Download necessary modules

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+	entry: {
+		js: './index.js'
+	},
+	output: { path: __dirname, filename: 'bundle.js' },
+	devtool: '#cheap-module-source-map',
+	devServer: {
+		historyApiFallback: true
+	},
+	resolve : {
+		extensions: ['.js', '.jsx']
+	},
+	module: {
+		rules: [{
+			test: /.jsx?$/,
+			use: ['babel-loader'],
+			exclude: /node_modules/
+
+		}]
+	},
+	plugins: [
+		new BrowserSyncPlugin({
+			host: 'localhost',
+			port: 3000,
+			proxy: 'http://localhost:8080/'
+		})
+	]
+}
