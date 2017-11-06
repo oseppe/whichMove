d9a1bbb20c8463c14d0714cc14e088b0120c4528
montalbo <oseppe@users.noreply.github.com>
2017-11-06 15:43:47 +0800

    Include chatentry component

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Chatscreen from './chatscreen'
+import ChatScreen from './components/chatScreen'
 
 render(
-	<Chatscreen />,
+	<ChatScreen />,
 	document.getElementById('app')
 )
---
+++ app/components/chatEntry.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// displayName = "anon", message = "sample message"
+
+export default function ChatEntry({displayName = "anon", message = "sample message"}) {
+	return (
+		<div style={{"marginBottom": 0}}>
+			<div></div>
+			<span style={{ "fontWeight": "bold", "paddingLeft": "5px" }}>{ displayName }</span>
+			<span style={{ "paddingLeft": "8px", "wordBreak": "break-all" }}>{ message }</span>
+		</div>
+	)
+}
